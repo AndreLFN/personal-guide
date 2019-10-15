18baed5b434e4b79da2462f4ad3890acc3195019
tarmontine <tarmontine@gmail.com>
2019-10-15 15:34:00 -0300

    update: gems

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_03_020114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alunos", force: :cascade do |t|
     t.string "nome"
     t.decimal "altura"
