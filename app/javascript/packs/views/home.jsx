d07dd196b547e8f2ef47bf5c06325b46c47ef53c
tarmontine <tarmontine@gmail.com>
2019-10-15 20:43:44 -0300

    added: heroku bootstrap support

--- app/javascript/packs/views/home.jsx
+++ app/javascript/packs/views/home.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import {render} from "react-dom";
 import Routes from "../../routes";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
 document.addEventListener("DOMContentLoaded", () => {
   render(
     Routes,
