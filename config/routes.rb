ee7a3a2d21001ee3b9ad4a55109fe704feeaead4
tarmontine <tarmontine@gmail.com>
2019-10-16 09:03:05 -0300

    update: reroutes ALL missing requests to React

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   # Reroutes all the other routes to react
-  get ':test', to: 'homepage#index'
+  get '*request', to: 'homepage#index'
 end
 
