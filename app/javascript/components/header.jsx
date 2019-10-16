396f08078bb3ea5be54d18a107a77490750dae3f
Nielx20 <jns3@cin.ufpe.br>
2019-10-16 16:08:44 -0300

    inserção da tela de login

--- app/javascript/components/header.jsx
+++ app/javascript/components/header.jsx
@@ -1,23 +1,21 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
       <div className="container secondary-color">
         <h1 className="display-4">Testing React</h1>
-        <p className="lead">
-        	This page was produced by React.
-        </p>
+        <p className="lead">This page was produced by React.</p>
         <hr className="my-4" />
-        <Link
-          to="/test/"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
+        <Link to="/test/" className="btn btn-lg custom-button" role="button">
           View
         </Link>
+        <Link
+          to="/singin/"
+          className="btn btn-lg custom-button"
+          role="button"
+        >Login</Link>
       </div>
     </div>
   </div>
--- app/javascript/routes.jsx
+++ app/javascript/routes.jsx
@@ -3,13 +3,14 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Home from "components/header";
 import Test from "components/test";
-
+import Singin from "components/singin";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/test" exact component={Test} />
+      <Route path="/singin" exact component={Singin} />
     </Switch>
   </Router>
 );
