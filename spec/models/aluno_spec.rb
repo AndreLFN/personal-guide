2556a9c2c73e0aa19e2bd9fab61fae389a5eba67
MauroAntonino <mauro_antonino@yahoo.com.br>
2019-10-30 19:09:33 -0300

    teste rspec

--- spec/models/aluno_spec.rb
+++ spec/models/aluno_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Aluno do
+<<<<<<< HEAD
 	subject  {described_class.new(nome: 'carlos',altura: 21,peso: 57, objetivo: 'emagrecer')}
 
 	describe 'nome' do
@@ -30,4 +31,27 @@ RSpec.describe Aluno do
 			expect(Aluno.all.size()).to eq(0)
 		end
 	end
+=======
+	subject  {described_class.new(nome: 'carlos',altura: 21,peso: 57, objetivo: 'emagrecer', cpf: 23)}
+
+	describe 'validação' do
+		describe 'nome' do
+			it 'deve estar presente' do
+				expect(subject).to be_valid
+			end
+		end
+
+		describe 'altura' do
+			it 'deve estar presente' do
+				expect(subject).to be_valid
+			end
+  		end
+
+  		describe 'objetivo' do
+  			it 'deve estar presente' do
+  				expect(subject).to be_valid
+  			end
+  		end
+  	end
+>>>>>>> teste rspec
 end
