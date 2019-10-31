3ccdcda6695eefab6247f9d38dd1d1d189164022
MauroAntonino <mauro_antonino@yahoo.com.br>
2019-10-31 00:42:39 -0300

    spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User do
+	subject  {described_class.new(encrypted_password: '123',email: "mimi@gmail.com")}
+
+	describe 'email' do
+		it 'deve estar presente' do
+			expect(subject).to be_valid
+		end
+	end
+
+	describe 'encrypted_password' do
+		it 'deve estar presente' do
+			expect(subject).to be_valid
+		end
+	end
+	describe 'tamanho' do
+		it 'deve estar presente' do
+			expect(Aluno.all.size()).to eq(0)
+		end
+	end
+end
+
+#RSpec.describe User, type: :model do
+# pending "add some examples to (or delete) #{__FILE__}"
 end
